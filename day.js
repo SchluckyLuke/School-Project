7e82c77afa339a37a5589f00b004ac35be987d4b
ShLuckyLuke <matthaeus.h24@gmail.com>
2026-03-10 21:42:19 +0100

    Add files via upload

--- day.js
+++ day.js
@@ -2,13 +2,99 @@ import { subjects } from "./dictionaries.js";
 
 const params = new URLSearchParams(window.location.search);
 
-const slot = params.get("slot");
-const subject = subjects[slot];
+const slot = params.get("slot").split("_");
+const subject = subjects[`${slot[0]}_${slot[1]}`];
 
-const date = new Date();
+/* ---------------------------------------------------------- */
 
-console.log(date.getMonth());
+const middle_childs = document.getElementsByClassName("middle_child");
+
+const dropdownA = document.querySelectorAll("#category_dropdown .dropdown-content a");
+const dropdownMenu = document.getElementsByClassName("dropdown-content");
+const dropdownButton = document.getElementById("dropdown-btn");
+const category_dropdown = document.getElementById("category_dropdown");
+
+const event_name = document.getElementById("event_input");
+const event_btn = document.getElementById("event-btn");
+const event_textarea = document.getElementById("event_textarea");
+
+let selectedCategory = null;
+let slotKey = `${slot[0]}_${slot[1]}_${slot[2]}_${slot[3]}_${slot[4]}`;
+let event_list = [];
 
 let subject_h1 = document.createElement("h1");
+subject_h1.classList.add("subject_title");
 subject_h1.textContent = subject;
-document.body.prepend(subject_h1);
+middle_childs[0].prepend(subject_h1);
+
+category_dropdown.addEventListener("mouseenter", () => {
+    dropdownMenu[0].style.display = "block";
+});
+
+category_dropdown.addEventListener("mouseleave", () => {
+    dropdownMenu[0].style.display = "none";
+});
+
+dropdownA.forEach(category =>{
+    category.addEventListener("click", event => {
+        dropdownButton.textContent = category.textContent;
+        selectedCategory = category.textContent;
+        dropdownMenu[0].style.display = "none";
+    })
+})
+
+event_btn.addEventListener("click", save_event);
+
+function save_event(){
+    let event_infos = {
+        name: event_name.value,
+        material: event_textarea.value,
+        category: selectedCategory,
+    }
+    if(event_infos.name != null && event_infos.category != null){
+        localStorage.setItem(JSON.stringify(event_infos), slotKey);
+        dropdownButton.textContent = "Kategorie wählen*";
+        event_name.value = "";
+        load_events();
+    }
+}
+
+function load_events(){
+    event_list = [];
+    middle_childs[1].innerHTML = "";
+    for(let i = 0; i < localStorage.length; i++){
+        if(localStorage.getItem(localStorage.key(i)) == slotKey){
+            event_list.push(localStorage.key(i));
+        }
+    }
+
+    event_list.forEach(event => {
+        let event_a = document.createElement("a");
+        let event_info = document.createElement("p");
+        let del_btn = document.createElement("button");
+        event_a.classList.add("event-a");
+        event_a.textContent = JSON.parse(event)["name"];
+        event_info.textContent = JSON.parse(event)["material"];
+        event_info.classList.add("event-info");
+        del_btn.classList.add("del-btn");
+        del_btn.textContent = "X";
+        del_btn.onclick = () => delete_events(event_a, event_info, event, del_btn);
+        middle_childs[1].appendChild(event_a);
+        middle_childs[1].appendChild(event_info);
+        middle_childs[1].appendChild(del_btn);
+    })
+
+    function delete_events(event_a, event_info, event, del_btn){
+        middle_childs[1].removeChild(event_a);
+        middle_childs[1].removeChild(event_info);
+        middle_childs[1].removeChild(del_btn)
+        let objectKey = {
+            name: event_a.textContent,
+            material: event_info.textContent,
+            category: JSON.parse(event)["category"]
+        }
+        localStorage.removeItem(JSON.stringify(objectKey));
+    }
+}
+
+load_events()
--- index.js
+++ index.js
@@ -1,51 +1,119 @@
 import { subjects } from "./dictionaries.js";
 
-function set_date(){
-    const date = new Date();
-    let date_h2 = document.getElementById("date_h2");
-    let date_month = "";
+let active_week = null;
 
-    switch(date.getMonth()){
-        case 0:
-            date_month = "Jänner";
-        
-        case 1:
-            date_month = "Ferbruar";
+const master_container = document.getElementById("master_container");
+let days_container = null;
 
-        case 2:
-            date_month = "März";
-    }
+let date_h2 = document.getElementById("date_h2");
 
-    date_h2.textContent = `${date.getDate()}. ${date_month} - ${date.getFullYear()}`;
+let left_button = document.getElementById("left_btn");
+let right_button = document.getElementById("right_btn");
+
+let get_date_list = null;
+
+left_button.addEventListener("click", week_back);
+right_button.addEventListener("click", week_forward);
+
+
+if(localStorage.getItem("activeWeek") == null){
+    localStorage.setItem("activeWeek", 0);
 }
 
-function create_days(){
-    let days_container = document.createElement("div");
+
+function get_date(week_num){
+
+    const today = new Date();
+
+    const monday = new Date(today);
+    monday.setDate(today.getDate() - ((today.getDay() || 7) - 1) + week_num);
+
+    const friday = new Date(monday);
+    friday.setDate(monday.getDate() + 4);
+
+    return [
+        monday.getDate(),
+        monday.getMonth()+1,
+        friday.getDate(),
+        friday.getMonth()+1,
+        monday.getFullYear()
+    ];
+}
+
+
+function create_days(slot_week_num){
+
+    const date = get_date(slot_week_num);
+
+    days_container = document.createElement("div");
     days_container.classList.add("days_container");
 
     for(let i = 1; i <= 5; i++){
+
         let hrs_container = document.createElement("div");
         hrs_container.classList.add("hrs_container");
 
         for(let e = 1; e <= 6; e++){
+
             let hr = document.createElement("a");
             hr.classList.add("hr");
-            hrs_container.appendChild(hr);
+
             let subject = subjects[`${i}_${e}`];
+
             if(subject == "Latein" || subject == "Französisch"){
                 hr.classList.add("blue_subject");
             }
+
             hr.textContent = subject;
-            hr.href = `day.html?slot=${i}_${e}`;
+            hr.href = `day.html?slot=${i}_${e}_${date[0]}_${date[2]}_${date[3]}`;
+
+            hrs_container.appendChild(hr);
         }
         days_container.appendChild(hrs_container);
     }
-    const master_container = document.getElementById("master_container");
     master_container.appendChild(days_container);
-
-
-
 }
 
-set_date()
-create_days()
+
+function week_back(){
+
+    let oldActiveWeek = Number(localStorage.getItem("activeWeek")) - 7;
+    localStorage.setItem("activeWeek", oldActiveWeek);
+
+    if(days_container){
+        master_container.removeChild(days_container);
+    }
+
+    create_days(oldActiveWeek);
+
+    const date = get_date(oldActiveWeek);
+
+    date_h2.textContent =
+    `${date[0]}. ${date[1]}. - ${date[2]}. ${date[3]}. ${date[4]}`;
+}
+
+
+function week_forward(){
+
+    let oldActiveWeek = Number(localStorage.getItem("activeWeek")) + 7;
+    localStorage.setItem("activeWeek", oldActiveWeek);
+
+    if(days_container){
+        master_container.removeChild(days_container);
+    }
+
+    create_days(oldActiveWeek);
+
+    const date = get_date(oldActiveWeek);
+
+    date_h2.textContent =
+    `${date[0]}. ${date[1]}. - ${date[2]}. ${date[3]}. ${date[4]}`;
+}
+
+
+get_date_list = get_date(Number(localStorage.getItem("activeWeek")));
+
+create_days(Number(localStorage.getItem("activeWeek")));
+
+date_h2.textContent =
+`${get_date_list[0]}. ${get_date_list[1]}. - ${get_date_list[2]}. ${get_date_list[3]}. ${get_date_list[4]}`;
